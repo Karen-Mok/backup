26598b83191710379c8f3c7c9418d0663c6c341d
Karen-Mok <31793132+Karen-Mok@users.noreply.github.com>
2022-07-11 21:48:46 +0800

    Update vultr-init-full.sh
    把需要下载的各种配置文件托管到github上改为固定地址了

--- shell/vultr-init-full.sh
+++ shell/vultr-init-full.sh
@@ -8,7 +8,7 @@ mkdir -p /home/transmission/downloads
 mkdir -p /home/transmission/tmp
 chmod -R 777 /home/transmission/
 mv /etc/transmission-daemon/settings.json /etc/transmission-daemon/settings.json.backup
-wget http://198.23.137.104:555/download/settings.json
+wget https://raw.githubusercontent.com/Karen-Mok/backup/master/%E5%85%B6%E4%BB%96%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6/settings.json
 sed  -i '51 s/\"\"/\"'${1}'\"/g' settings.json    #参数为密码,bash init.sh password
 mv settings.json /etc/transmission-daemon/
 wget https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh 
@@ -20,7 +20,7 @@ cd ~
 service transmission-daemon start
 
 
-wget http://198.23.137.104:555/download/aria2.sh   # 下载aria2.sh 但不安装
+wget https://raw.githubusercontent.com/Karen-Mok/backup/master/shell/aria2.sh   # 下载aria2.sh 但不安装
 wget -N --no-check-certificate "https://github.com/mayswind/AriaNg/releases/download/0.4.0/aria-ng-0.4.0.zip" 
 # 下载AriaNg
 rm -f /var/www/html/*
@@ -28,12 +28,6 @@ mv aria-ng-0.4.0.zip /var/www/html/
 cd /var/www/html
 unzip aria-ng-0.4.0.zip
 cd ~
-
-wget https://github.com/gphotosuploader/gphotos-uploader-cli/releases/download/v1.0.6/gphotos-uploader-cli_1.0.6_linux_amd64.tar.gz
-tar -xvzf gphotos-uploader-cli_1.0.6_linux_amd64.tar.gz   # 下载解压谷歌相册上传程序
-
-wget https://github.com/timvisee/ffsend/releases/download/v0.2.59/ffsend-v0.2.59-linux-x64-static
-mv ffsend-v0.2.59-linux-x64-static ffsend  # 下载ffsend
 
 wget https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.tgz
 tar -xvzf gotop_3.0.0_linux_amd64.tgz
@@ -46,14 +40,20 @@ sysctl -p
 #unzip BaiduPCS-Go-v3.5.6-linux-amd64.zip
 #mv BaiduPCS-Go-v3.5.6-linux-amd64/BaiduPCS-Go .    #原版本已经不可用,上传有问题会失败
 
-wget https://ci.appveyor.com/api/buildjobs/6pl7a26vm6lko92v/artifacts/BaiduPCS-Go-linux-amd64.zip
-unzip BaiduPCS-Go-linux-amd64.zip
+wget https://github.com/qjfoidnh/BaiduPCS-Go/releases/download/v3.8.7/BaiduPCS-Go-v3.8.7-linux-amd64.zip
+unzip BaiduPCS-Go-v3.8.7-linux-amd64.zip
+
+wget https://github.com/tickstep/cloudpan189-go/releases/download/v0.1.1/cloudpan189-go-v0.1.1-linux-amd64.zip
+unzip cloudpan189-go-v0.1.1-linux-amd64.zip
+
+wget https://github.com/tickstep/aliyunpan/releases/download/v0.1.8/aliyunpan-v0.1.8-linux-amd64.zip
+unzip aliyunpan-v0.1.8-linux-amd64.zip
 
 echo "export PATH=/root/:$PATH" >> /etc/profile
 source /etc/profile
 
-echo '/dev/vdb1 /home/transmission/downloads ext4   defaults    0  0' >> /etc/fstab   
-mount -a
+#echo '/dev/vdb1 /home/transmission/downloads ext4   defaults    0  0' >> /etc/fstab   
+#mount -a
 
 mkdir /var/www/html/pt
 ln -s /home/transmission/downloads /var/www/html/pt
